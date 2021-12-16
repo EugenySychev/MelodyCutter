d22087c5209e5e9c98fae720e6d406d3d48ab350
EugenySychev <eugeny.sychev@gmail.com>
2021-12-16 18:33:10 +0300

    some cleanup

--- app/src/main/java/com/sychev/melodycutter/MainActivity.kt
+++ app/src/main/java/com/sychev/melodycutter/MainActivity.kt
@@ -13,7 +13,9 @@ import com.sychev.melodycutter.databinding.ActivityMainBinding
 import android.content.Intent
 import android.net.Uri
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toFile
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +24,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        if (uri != null) {
+            m_fileName = uri
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,30 +37,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_open -> {
                 callOpenFileDialog()
+                true
+            }
+            R.id.action_save -> {
+                // TODO: add save file action
+                Toast.makeText(this, "You select save item", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.action_crop -> {
+                // TODO: check selection and crop it
+                Toast.makeText(this, "You select crop item", Toast.LENGTH_LONG).show()
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -60,19 +65,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun callOpenFileDialog() {
-        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-            if (uri != null) {
-                m_fileName = uri
-            }
-            Log.d("Main", "Item ")
-        }
 
-        getContent.launch("sound/*")
+        getContent.launch("image/*")
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
